6d860260e6be568d8e662414dd706598a19b0bb3
Philip Petev <philip.petev@gmail.com>
2013-09-13 10:23:09 +0300

    ProBook Installer 6.1 beta 9

--- BlinkScreen/blinkscreen.sh
+++ BlinkScreen/blinkscreen.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-# install to ~/autoexec.sh
-nvram -d boot-args
 # might not be necessary to sleep for a second, you can experiment with/without it...
 sleep 1
 # blink the screen, fixing the brightness issue...
